75f9f4f93480a7312185856fd4a8886be6d0d208
6ui4r4uj0 <62915262+GuiAralopes@users.noreply.github.com>
2022-12-08 00:58:05 -0300

    Create hospital_scripts.sql

---
+++ scripts/hospital_scripts.sql
@@ -0,0 +1,3 @@
+insert into HOSPITAL(IDHOSPITAL, NOME_HOSPITAL, ENDERECO_COMPLETO, CEP, CNPJ)
+values( 1, 'H. Casa do Chapéu','Rua Humberto Serrano', 29101555, 12312312312312);
+insert into HOSPITAL values( 2, 'H. Casa do Carai','Rua Almir Pereira', 29101555, 23423423423423);
