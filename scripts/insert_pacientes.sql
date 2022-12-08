da1cef3d0c4d5f5a525605c52ae2abd4b55e719c
6ui4r4uj0 <62915262+GuiAralopes@users.noreply.github.com>
2022-12-08 01:00:22 -0300

    Create insert_pacientes.sql

---
+++ scripts/insert_pacientes.sql
@@ -0,0 +1,3 @@
+insert into PACIENTE (IDPACIENTE, IDHOPSITAL, PRIMEIRO_NOME, NOME_MEIO, ULTIMO_NOME, DATA_NASCIMENTO, CPF, TELEFONE, LEITO, SESSAO, NOME_PARENTE,TELEFONE_PARENTE)
+values                      ('', 1, 'Julio', 'Pinto', 'Cocielo', '02/02/1990', 12312312312, 27999999999, 'uti', 'Covid-19', 'Claudio da Silva Pereira Sampaio',27999335544);
+insert into PACIENTE values('', 2, 'Carlos', 'Alberto', 'Nóbrega', '03/02/1999', 45645645645, 27888888888, 'uti', 'Acidentes', 'Ednaldo Pereira',27999883322);
